224d15a4d28baca803b7f74981e952f3dc51ae22
Albane Escudero <albane.escudero@epfedu.fr>
2025-10-24 12:01:39 +0200

    Création d'un nouveau projet TP0_calculette

--- TP0_calculette/src/tp0_calculette/TP0_calculette.java
+++ TP0_calculette/src/tp0_calculette/TP0_calculette.java
@@ -14,13 +14,12 @@ public class TP0_calculette {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println(Please enter the operator:); 
-1) add
-2) substract
-3) multiply
-4) divide
-5) modulo
-
+        System.out.println("Please enter the operator:");
+        System.out.println("1) add");
+        System.out.println("2) substract");
+        System.out.println("3) multiply");
+        System.out.println("4) divide");
+        System.out.println("5) modulo");
     }
     
 }
