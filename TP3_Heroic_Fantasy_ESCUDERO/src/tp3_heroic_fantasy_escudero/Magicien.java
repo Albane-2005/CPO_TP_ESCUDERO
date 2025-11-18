e5eee3e153f707e4590c1569254f530bd38363c5
Albane Escudero <albane.escudero@epfedu.fr>
2025-11-18 12:03:25 +0100

    Création d'un nouveau projet TP3_Heroic_Fantasy

---
+++ TP3_Heroic_Fantasy_ESCUDERO/src/tp3_heroic_fantasy_escudero/Magicien.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp3_heroic_fantasy_escudero;
+
+/**
+ *
+ * @author alban
+ */
+public class Magicien extends Personnage {
+
+    private boolean confirme;
+
+    public Magicien(String nom, int niveauVie, boolean confirme) {
+        super(nom, niveauVie);
+        this.confirme = confirme;
+    }
+
+    public void setConfirme(boolean confirme) {
+        this.confirme = confirme;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", magicien confirmé : " + confirme;
+    }
+    
+}
